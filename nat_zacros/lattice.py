c7ef2715a42f80b6d3f79c12995e793d4a5cd834
akandra <akandra@mpibpc.mpg.de>
2026-01-06 18:14:55 +0100

    continue checking

--- nat_zacros/lattice.py
+++ nat_zacros/lattice.py
@@ -113,6 +113,7 @@ class lattice:
 
         if self.folder is None:
             print('nothing to get: lattice folder not defined')
+            print('test printing')
             return
 
         self.folder = Path(self.folder)
